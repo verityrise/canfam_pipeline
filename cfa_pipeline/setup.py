48809bb9e83de5cbf42b6eef600179e6d1260db7
hzl <verityrise@gmail.com>
2019-01-05 14:31:38 +1100

    Update setup.py

--- cfa_pipeline/setup.py
+++ cfa_pipeline/setup.py
@@ -3,22 +3,22 @@
 from setuptools import setup
 
 setup(
-    name='fampipeline',
+    name='cfa_pipeline',
     version='0.0.1',
-    author='Bernie Pope, Khalid Mahmood, Jessica Chung',
-    author_email='kmahmood@unimelb.edu.au',
+    author='Bernie Pope, Khalid Mahmood, Jessica Chung, Hongzhi Luo',
+    author_email='kmahmood@unimelb.edu.au, hongzhil@unimelb.edu.au',
     packages=['src'],
     entry_points={
-        'console_scripts': ['fampipeline = src.main:main']
+        'console_scripts': ['cfapipeline = src.main:main']
     },
-    url='https://github.com/khalidm/fampipeline',
+    url='https://github.com/verityrise/canfam_pipeline',
     license='LICENSE.txt',
-    description='fampipeline is a pipeline system for bioinformatics workflows\
+    description='cfapipeline is a pipeline system for bioinformatics workflows\
      with support for running pipeline stages on a distributed compute cluster.',
     long_description=open('README.md').read(),
     install_requires=[
         "ruffus == 2.6.3",
         "drmaa == 0.7.6",
-        "PyYAML == 3.11"
+        "pyyaml>=4.2b1"
     ],
 )
